bd91bedbe6fd69c9648441190d4ced0df9f5e693
JackFurby <jackfurby@live.co.uk>
2021-03-14 01:13:00 +0000

    Delete config.py
    Replaced with configExample.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-
-class Config(object):
-	SECRET_KEY = os.environ.get('SECRET_KEY') or '1j6EBSGw0tDHl3/Udm6i3NYMz+wrnnvUjij+ouCpPlSjXDgA7z2UW7akR9gS5ZzC9gGfiC5gMpwPsV6puKnfug=='
-	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
-	SQLALCHEMY_TRACK_MODIFICATIONS = False
