1e1d4139379f1ad4f71c5d3c5fcfb24c773b724b
Jake Amey <ameyj2@cardiff.ac.uk>
2021-06-17 12:54:48 +0000

    Added hacky fix to stop weather call fro breaking, will implement proper fix shortly

--- reporter_app/electricity_use/routes.py
+++ reporter_app/electricity_use/routes.py
@@ -8,7 +8,8 @@ from flask_security import auth_required, roles_required
 
 
 def call_leccyfunc():
-    weather = getWeather(16, 'OWM').full_df
+    # HACKY FIX TODO (Jake): add script to ensure this calls the next nearest hour 
+    weather = getWeather(24, 'OWM').full_df
     temp = weather[['time', 'temp']]
     leccy_df = pd.DataFrame(columns=['Time', 'Electricity Usage (Kw)'])
     for i, time in enumerate(weather.time):
