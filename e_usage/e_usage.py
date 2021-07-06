4ecc394dc4e83c015e5618a494cc13a01197ca0e
Jack Furby <jackfurby@live.co.uk>
2021-07-06 12:56:04 +0100

    corrected table name

--- e_usage/e_usage.py
+++ e_usage/e_usage.py
@@ -9,7 +9,7 @@ from reporter_app.electricity_use.utils import call_leccyfunc
 engine = db.create_engine(os.environ.get('AIMLACKIES_REPORTER_DATABASE_URL'))
 connection = engine.connect()
 metadata = db.MetaData()
-e_use_table = db.Table('electricity_use', metadata, autoload=True, autoload_with=engine)
+e_use_table = db.Table('e_use', metadata, autoload=True, autoload_with=engine)
 
 #grab data
 e_use_df = call_leccyfunc()
@@ -19,5 +19,3 @@ for idx, row in e_use_df.iterrows():
     query = db.insert(e_use_table).values(date_time=row['Time'],
             electricity_use=row['Electricity Usage (kW)'])
     ResultProxy = connection.execute(query)
-
-
