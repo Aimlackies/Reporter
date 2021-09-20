0dd51d23ddd3648a8b9df67194c9170aded99ba5
Jack Furby <jackfurby@live.co.uk>
2021-09-20 17:02:45 +0100

    added real electricity usage data to power usage page

--- reporter_app/electricity_use/routes.py
+++ reporter_app/electricity_use/routes.py
@@ -3,7 +3,7 @@ from reporter_app.electricity_use import bp
 from reporter_app import db
 from reporter_app.models import User, ElecUse
 import pandas as pd
-from reporter_app.electricity_use.utils import call_leccyfunc
+from reporter_app.electricity_use.utils import call_leccyfunc, get_real_power_usage_for_times
 from flask_security import auth_required, roles_required
 from datetime import datetime, timedelta
 
@@ -53,5 +53,12 @@ def e_use_28d():
 @roles_required('verified')
 def e_use_all():
     e_use_entries = ElecUse.query.all()
+
+    labels = [row.date_time for row in e_use_entries]
+    power_e_use_data = [row.electricity_use for row in e_use_entries]
+
+    real_power_usage = get_real_power_usage_for_times(labels)
     return render_template('electricity_use/electricity_use.html',
-                           e_use_entries=e_use_entries)
+                           data_labels=labels,
+                           power_e_use_data=power_e_use_data,
+                           real_power_usage=real_power_usage)
